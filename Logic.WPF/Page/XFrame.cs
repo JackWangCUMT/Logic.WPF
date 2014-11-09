cb1e6814dfc99f1ecc6a8bd1dc5b4c6b92d7ee28
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-11-09 13:58:17 +0100

    Added frame headers and numbers

--- Logic.WPF/Page/XFrame.cs
+++ Logic.WPF/Page/XFrame.cs
@@ -34,6 +34,188 @@ namespace Logic.WPF.Page
 
             _shapes = new ObservableCollection<IShape>();
 
+            // headers
+            _shapes.Add(
+                new XText()
+                {
+                    X = 0.0,
+                    Y = 0.0,
+                    Width = 330.0,
+                    Height = 30.0,
+                    HAlignment = HAlignment.Center,
+                    VAlignment = VAlignment.Center,
+                    FontName = "Consolas",
+                    FontSize = 19.0,
+                    Text = "I N P U T S"
+                });
+            _shapes.Add(
+                new XText()
+                {
+                    X = 30.0 + 5.0,
+                    Y = 30.0 + 0.0,
+                    Width = 200.0,
+                    Height = 15.0,
+                    HAlignment = HAlignment.Left,
+                    VAlignment = VAlignment.Center,
+                    FontName = "Consolas",
+                    FontSize = 11.0,
+                    Text = "Designation"
+                });
+            _shapes.Add(
+                new XText()
+                {
+                    X = 30.0 + 5.0,
+                    Y = 30.0 + 15.0,
+                    Width = 200.0,
+                    Height = 15.0,
+                    HAlignment = HAlignment.Left,
+                    VAlignment = VAlignment.Center,
+                    FontName = "Consolas",
+                    FontSize = 11.0,
+                    Text = "Description"
+                });
+            _shapes.Add(
+                new XText()
+                {
+                    X = 30.0 + 215.0,
+                    Y = 30.0 + 0.0,
+                    Width = 80.0,
+                    Height = 15.0,
+                    HAlignment = HAlignment.Left,
+                    VAlignment = VAlignment.Center,
+                    FontName = "Consolas",
+                    FontSize = 11.0,
+                    Text = "Signal"
+                });
+            _shapes.Add(
+                new XText()
+                {
+                    X = 30.0 + 215.0,
+                    Y = 30.0 + 15.0,
+                    Width = 80.0,
+                    Height = 15.0,
+                    HAlignment = HAlignment.Left,
+                    VAlignment = VAlignment.Center,
+                    FontName = "Consolas",
+                    FontSize = 11.0,
+                    Text = "Condition"
+                });
+            _shapes.Add(
+                new XText()
+                {
+                    X = 330.0,
+                    Y = 0.0,
+                    Width = 600.0,
+                    Height = 30.0,
+                    HAlignment = HAlignment.Center,
+                    VAlignment = VAlignment.Center,
+                    FontName = "Consolas",
+                    FontSize = 19.0,
+                    Text = "F U N C T I O N"
+                });
+            _shapes.Add(
+                new XText()
+                {
+                    X = 930.0,
+                    Y = 0.0,
+                    Width = 330.0,
+                    Height = 30.0,
+                    HAlignment = HAlignment.Center,
+                    VAlignment = VAlignment.Center,
+                    FontName = "Consolas",
+                    FontSize = 19.0,
+                    Text = "O U T P U T S"
+                });
+            _shapes.Add(
+                new XText()
+                {
+                    X = 930.0 + 5.0,
+                    Y = 30.0 + 0.0,
+                    Width = 200.0,
+                    Height = 15.0,
+                    HAlignment = HAlignment.Left,
+                    VAlignment = VAlignment.Center,
+                    FontName = "Consolas",
+                    FontSize = 11.0,
+                    Text = "Designation"
+                });
+            _shapes.Add(
+                new XText()
+                {
+                    X = 930.0 + 5.0,
+                    Y = 30.0 + 15.0,
+                    Width = 200.0,
+                    Height = 15.0,
+                    HAlignment = HAlignment.Left,
+                    VAlignment = VAlignment.Center,
+                    FontName = "Consolas",
+                    FontSize = 11.0,
+                    Text = "Description"
+                });
+            _shapes.Add(
+                new XText()
+                {
+                    X = 930.0 + 215.0,
+                    Y = 30.0 + 0.0,
+                    Width = 80.0,
+                    Height = 15.0,
+                    HAlignment = HAlignment.Left,
+                    VAlignment = VAlignment.Center,
+                    FontName = "Consolas",
+                    FontSize = 11.0,
+                    Text = "Signal"
+                });
+            _shapes.Add(
+                new XText()
+                {
+                    X = 930.0 + 215.0,
+                    Y = 30.0 + 15.0,
+                    Width = 80.0,
+                    Height = 15.0,
+                    HAlignment = HAlignment.Left,
+                    VAlignment = VAlignment.Center,
+                    FontName = "Consolas",
+                    FontSize = 11.0,
+                    Text = "Condition"
+                });
+
+            // numbers
+            double lx = 0.0;
+            double ly = 60.0;
+            double rx = 1230.0;
+            double ry = 60.0;
+            for (int n = 1; n <= 24; n++)
+            {
+                _shapes.Add(
+                    new XText()
+                    {
+                        X = lx,
+                        Y = ly,
+                        Width = 30.0,
+                        Height = 30.0,
+                        HAlignment = HAlignment.Center,
+                        VAlignment = VAlignment.Center,
+                        FontName = "Consolas",
+                        FontSize = 15.0,
+                        Text = n.ToString("00")
+                    });
+                _shapes.Add(
+                    new XText()
+                    {
+                        X = rx,
+                        Y = ry,
+                        Width = 30.0,
+                        Height = 30.0,
+                        HAlignment = HAlignment.Center,
+                        VAlignment = VAlignment.Center,
+                        FontName = "Consolas",
+                        FontSize = 15.0,
+                        Text = n.ToString("00")
+                    });
+                ly += 30.0;
+                ry += 30.0;
+            }
+
             _shapes.Add(new XLine() { X1 = 0.0, Y1 = 0.0, X2 = 1260.0, Y2 = 0.0 });
             _shapes.Add(new XLine() { X1 = 0.0, Y1 = 30.0, X2 = 1260.0, Y2 = 30.0 });
             _shapes.Add(new XLine() { X1 = 0.0, Y1 = 780.0, X2 = 1260.0, Y2 = 780.0 });
