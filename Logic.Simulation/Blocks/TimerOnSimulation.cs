b5ed0419380202a4ea2573e2843b557bc46cd4af
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-11-16 21:05:05 +0100

    Added timer on simulation

--- Logic.Simulation/Blocks/TimerOnSimulation.cs
+++ Logic.Simulation/Blocks/TimerOnSimulation.cs
@@ -20,9 +20,64 @@ namespace Logic.Simulation.Blocks
             this.Delay = delay;
         }
 
+        private bool _isEnabled;
+        private long _endCycle;
+
         public override void Run(IClock clock)
         {
-            // TODO: Implement simulation.
+            int length = Inputs.Length;
+            if (length == 0)
+            {
+                // Do nothing.
+            }
+            else if (length == 1)
+            {
+                var input = Inputs[0];
+                bool? enableState = input.IsInverted ? !(input.Simulation.State) : input.Simulation.State;
+                switch (enableState)
+                {
+                    case true:
+                        {
+                            if (_isEnabled)
+                            {
+                                if (clock.Cycle >= _endCycle && base.State != true)
+                                {
+                                    base.State = true;
+                                }
+                            }
+                            else
+                            {
+                                // Delay -> in seconds
+                                // Clock.Cycle
+                                // Clock.Resolution -> in milliseconds
+                                long cyclesDelay = (long)(Delay * 1000.0) / clock.Resolution;
+                                _endCycle = clock.Cycle + cyclesDelay;
+                                _isEnabled = true;
+                                if (clock.Cycle >= _endCycle)
+                                {
+                                    base.State = true;
+                                }
+                            }
+                        }
+                        break;
+                    case false:
+                        {
+                            _isEnabled = false;
+                            base.State = false;
+                        }
+                        break;
+                    case null:
+                        {
+                            _isEnabled = false;
+                            base.State = null;
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                throw new Exception("TimerOn simulation can only have one input State.");
+            }
         }
     }
 }
