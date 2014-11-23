ba41fb82d1942b073a1cf0f4586cbead3b02e095
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-11-23 17:43:30 +0100

    Added IPage interface

---
+++ Logic.Core/IPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic.Core
+{
+    public interface IPage
+    {
+        string Name { get; set; }
+        ITemplate Template { get; set; }
+        IList<IShape> Shapes { get; set; }
+        IList<IShape> Blocks { get; set; }
+        IList<IShape> Pins { get; set; }
+        IList<IShape> Wires { get; set; }
+    }
+}
