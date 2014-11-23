09fd05ac458ad9ec73b224c3b5d64ad784376100
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-11-23 17:43:41 +0100

    Added IProject interface

---
+++ Logic.Core/IProject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic.Core
+{
+    public interface IProject
+    {
+        string Name { get; set; }
+        IList<IStyle> Styles { get; set; }
+        IList<ITemplate> Templates { get; set; }
+        IList<IDocument> Documents { get; set; }
+    }
+}
