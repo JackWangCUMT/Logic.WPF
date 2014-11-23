2452c5524d8009edf4e3e6fa2cc63fac5d100055
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-11-23 17:43:20 +0100

    Added IDocument interface

---
+++ Logic.Core/IDocument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic.Core
+{
+    public interface IDocument
+    {
+        string Name { get; set; }
+        IList<IPage> Pages { get; set; }
+    }
+}
