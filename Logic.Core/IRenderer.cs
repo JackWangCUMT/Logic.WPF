0202793fa3365ee668b9d44d6f26279118afef2a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-12-02 19:32:21 +0100

    Added image renderer interface

--- Logic.Core/IRenderer.cs
+++ Logic.Core/IRenderer.cs
@@ -15,6 +15,7 @@ namespace Logic.Core
         void DrawEllipse(object dc, IStyle style, XEllipse ellipse);
         void DrawRectangle(object dc, IStyle style, XRectangle rectangle);
         void DrawText(object dc, IStyle style, XText text);
+        void DrawImage(object dc, IStyle style, XImage image);
         void DrawPin(object dc, IStyle style, XPin pin);
         void DrawWire(object dc, IStyle style, XWire wire);
     }
