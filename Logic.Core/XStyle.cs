5c3cc52bcf1fcbc5cb41a94c2284dc104a6e263d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-12-06 20:15:36 +0100

    Added XStyle class

---
+++ Logic.Core/XStyle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic.Core
+{
+    public class XStyle : IStyle
+    {
+        public string Name { get; set; }
+        public IColor Fill { get; set; }
+        public IColor Stroke { get; set; }
+        public double Thickness { get; set; }
+        public object NativeFill() { return null; }
+        public object NativeStroke() { return null; }
+    }
+}
