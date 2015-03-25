363bc269b3405fceed8b05c99400716cb47db671
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2015-03-25 12:07:38 +0100

    Update NativeFileDialog.cs

--- Logic.WPF/Native/NativeFileDialog.cs
+++ Logic.WPF/Native/NativeFileDialog.cs
@@ -1,4 +1,6 @@
-﻿using Logic.Portable;
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Logic.Portable;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
