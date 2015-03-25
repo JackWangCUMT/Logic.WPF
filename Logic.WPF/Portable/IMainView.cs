d8ae09700b2aa2122198cb0b7f1ace01f45357bd
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2015-03-25 12:08:53 +0100

    Update IMainView.cs

--- Logic.WPF/Portable/IMainView.cs
+++ Logic.WPF/Portable/IMainView.cs
@@ -1,4 +1,6 @@
-﻿using Logic.ViewModels;
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Logic.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
