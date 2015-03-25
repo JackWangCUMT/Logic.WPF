36197908c31ace1d59e28ab29d597fea77375f3d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2015-03-25 12:06:07 +0100

    Update Output.cs

--- Logic.WPF/Blocks/Signal/Output.cs
+++ Logic.WPF/Blocks/Signal/Output.cs
@@ -1,3 +1,5 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using Logic.Core;
 using System;
 using System.Collections.Generic;
